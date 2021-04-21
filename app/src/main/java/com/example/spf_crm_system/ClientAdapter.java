99273847e10eaed1490cadb1982aca9ec9c32753
майа хуху <dariya.bigabulova@gmail.com>
2021-04-21 14:50:50 +0600

    clients page

---
+++ app/src/main/java/com/example/spf_crm_system/ClientAdapter.java
@@ -0,0 +1,49 @@
+package com.example.spf_crm_system;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ClientAdapter extends RecyclerView.Adapter<ClientAdapter.ViewHolder> {
+    private final LayoutInflater inflater;
+    List<Client> clients;
+
+    public ClientAdapter(Context context, List<Client> clients) {
+        this.inflater = LayoutInflater.from(context);
+        this.clients=clients;
+    }
+
+    @NonNull
+    @Override
+    public ClientAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.clients, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClientAdapter.ViewHolder holder, int position) {
+        Client client=clients.get(position);
+        holder.name.setText(client.getFirst_name()+" "+client.last_name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return clients.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        //final ImageView imageView;
+        final TextView name;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name=(TextView) itemView.findViewById(R.id.textView);
+        }
+    }
+}
--- app/src/main/java/com/example/spf_crm_system/MainActivity2.kt
+++ app/src/main/java/com/example/spf_crm_system/MainActivity2.kt
@@ -2,10 +2,21 @@ package com.example.spf_crm_system
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val clients:List <Client>
+
+        val client=Client(1,"Dariya","Bigabulova","d@email.ru","87478202172")
+
+        clients= listOf(client)
+
+        val clients_list=findViewById<RecyclerView>(R.id.clients_list)
+        val clientsAdapter=ClientAdapter(this,clients)
     }
+
 }
