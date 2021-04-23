f773777dfd837e78467f2b528f215dd817bccd6f
майа хуху <dariya.bigabulova@gmail.com>
2021-04-23 17:19:12 +0600

    product page

--- app/src/main/java/com/example/spf_crm_system/ProductActivity.kt
+++ app/src/main/java/com/example/spf_crm_system/ProductActivity.kt
@@ -13,7 +13,12 @@ class ProductActivity : AppCompatActivity() {
         val products:List <Product>
 
 
-        products= listOf(Product(1,"на картриджи Сanon 725, 728, 737, 712; HP 278, 285",100,"Фотобарабан HP P1005"))
+        products= mutableListOf(Product(1,"на картриджи Сanon 725, 728, 737, 712; HP 278, 285",100,"Фотобарабан HP P1005"),
+        Product(2,"HP Q2612/FX9/FX10",800,"Фотобарабан HP 1010"),
+            Product(3,"Canon MF 3220/HP 7115",800,"Фотобарабан HP 1200"),
+            Product(4,"Q2612/FX9",800,"Тонер MPT9, 100гр"),
+            Product(5,"HP 285/278; Canon 725/728/737/712",900,"Тонер MPT10, 80гр")
+            )
 
         val products_list=findViewById<RecyclerView>(R.id.product_list)
         val productsAdapter=ProductAdapter(this,products)
